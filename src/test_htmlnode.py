1b335252b49a09c8d884667778288fd3004eb1de
4-82 <180662930+4-82@users.noreply.github.com>
2025-06-02 10:03:18 -0400

    htmlnode: text_node_to_html_node

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
-from htmlnode import HTMLNode
-
+from htmlnode import HTMLNode, text_node_to_html_node
+from textnode import TextType, TextNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html_props(self):
@@ -48,7 +48,36 @@ class TestHTMLNode(unittest.TestCase):
             node.__repr__(),
             "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
         )
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+        
 
 if __name__ == "__main__":
     unittest.main()
