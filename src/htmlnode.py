930e9d0a3480cb9df4aaab928cdac9fd0ae2c15f
4-82 <180662930+4-82@users.noreply.github.com>
2025-06-02 10:18:44 -0400

    htmlnode: textnode: cleanup functions + tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -48,18 +48,3 @@ class ParentNode(HTMLNode):
     def __repr__(self):
         return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
 
-def text_node_to_html_node(text_node):
-    match text_node.text_type.value:
-        case TextType.TEXT.value:
-          return LeafNode(None, text_node.text)
-        case TextType.BOLD.value:
-            return LeafNode("b", text_node.text)
-        case TextType.ITALIC.value:
-            return LeafNode("i", text_node.text)
-        case TextType.CODE.value:
-            return LeafNode("code", text_node.text)
-        case TextType.LINK.value:
-            return LeafNode("a", text_node.text, "href")
-        case TextType.IMAGE.value:
-            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
-        raise Exception("Type is not in TextType")
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+from htmlnode import HTMLNode, LeafNode
 from enum import Enum
 
 class TextType(Enum):
@@ -23,3 +24,21 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type.value:
+        case TextType.TEXT.value:
+          return LeafNode(None, text_node.text)
+        case TextType.BOLD.value:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC.value:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE.value:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK.value:
+            return LeafNode("a", text_node.text, "href")
+        case TextType.IMAGE.value:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError("Type is not in TextType")
+
