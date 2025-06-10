117a9b5751b4838461a4bfbae8dc048a52fc3e2b
4-82 <180662930+4-82@users.noreply.github.com>
2025-06-10 05:32:15 -0400

    blocks: add block_to_block_type

--- src/blocks.py
+++ src/blocks.py
@@ -12,3 +12,30 @@ def markdown_to_blocks(markdown):
     result = list(filter(None, map(lambda x: x.strip(), text.split("\n\n"))))
     return result
 
+
+def block_to_block_type(block):
+    if (block.startswith("#")):
+        return BlockType.HEADING.value
+    elif (block[:3] == block[-3:]) and block.startswith("`"):
+        return BlockType.CODE.value
+    elif (block.startswith(">")):
+         if False in list(map(lambda x: x[0] == ">",(block.split("\n")))):
+             return False
+         return BlockType.QUOTE.value
+    elif (block.startswith("-")):
+         if False in list(map(lambda x: x[0] == "-" and x[1] == " ", (block.split("\n")))):
+             return False
+         return BlockType.UNORDERED_LIST.value
+    elif (block[:3] == "1. "):
+        count = 0
+        for str in block.split("\n"):
+            if int(str[0]) == (count + 1) and str[1:3] == ". ":
+                count += 1
+                result = True
+            else:
+                result = False
+        if result == True:
+            return BlockType.ORDERED_LIST.value            
+    return BlockType.PARAGRAPH.value
+
+print(block_to_block_type("1. this is a test\n2. okay \n3. sure"))
