0d77d91ef87ce96aebd53e412ef80fdfe9c79a0a
4-82 <180662930+4-82@users.noreply.github.com>
2025-06-16 17:41:21 -0400

    main: add handle_file_recursive

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
+import os, shutil
 from textnode import TextNode, TextType
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://boot.dev")
-    print(node.__repr__())
+    pass
 
-main()
+def handle_file_recursive(source="./static", destination="/home/alice/projects/bootdev/static-site-generator/public"):
+    if not (os.path.exists(destination)):
+        os.mkdir(destination)
+    for content in (os.listdir(source)):
+        filepath = os.path.join(source, content)
+        if (os.path.isfile(filepath)):
+            shutil.copy2(filepath, destination)
+        else:
+            handle_file_recursive(os.path.join(source, content), os.path.join(destination, content))
+    
+handle_file_recursive()
