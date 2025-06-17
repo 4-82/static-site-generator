02739bbf3604aef95ea88ae03d040854ada3ee73
4-82 <180662930+4-82@users.noreply.github.com>
2025-06-17 18:47:11 -0400

    main: generate_page: close files after opening

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os, shutil
+from blocks import markdown_to_html_node
 
 dir_path_static = "./static"
 dir_path_public = "./public"
@@ -30,4 +31,37 @@ def handle_file_recursive(source="./static", destination="/home/alice/projects/b
         else:
             handle_file_recursive(os.path.join(source, content), os.path.join(destination, content))
     
-handle_file_recursive()
+def extract_markdown_title(markdown):
+    doc = markdown.split("\n")
+    for line in doc:
+        if ((line.startswith("#")) and (line.split(" ").count("#") == 1)):
+            string = line.split(" ")
+            string.remove("#")
+            string = " ".join(string)
+            string.strip()
+    return string
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}...")
+
+    from_file = open(from_path)
+    from_file_text = from_file.read()
+    from_file.close()
+
+    template = open(template_path)
+    template_file_text = template.read()
+    template.close()
+
+    file = markdown_to_html_node(from_file_text)
+    file = file.to_html()
+    title = extract_markdown_title(from_file_text)
+    template = template_file_text.replace("{{ Title }}", title)
+    template = template_file_text.replace("{{ Content }}", file)
+    if not (os.path.exists(dest_path)):
+        os.makedirs(os.path.dirname(dest_path))
+    if os.path.exists(os.path.join(dest_path, "index.html")):
+        pass
+    else:
+        with open(os.path.join(dest_path, "index.html"), "w") as page:
+            page.write(template_file_text)
+        page.close()
